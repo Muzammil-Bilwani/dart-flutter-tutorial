e85a864fb63d2c37b354ea32a946ae48c7651a6b
Muzammil Bilwani <muzammil.bilwani@hotmail.com>
2023-02-19 00:44:22 +0500

    Added comments for understanding

--- week0/program.dart
+++ week0/program.dart
@@ -2,8 +2,9 @@ void main() {
   String text = "BANO QABIL";
 
   print("Text Before Reversing: $text");
-  print("Text After Reversing: ");
 
+  // text length -> 9
+  // text length - 1 -> 8
   for (var i = text.length - 1; i >= 0; i--) {
     print(text[i]);
   }
