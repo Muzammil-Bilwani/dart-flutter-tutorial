a5f037c122827e610761e2642128e46f321e9e77
Muzammil Bilwani <muzammil.bilwani@hotmail.com>
2023-02-26 02:09:24 +0500

    Updated Code

--- week_one/program.dart
+++ week_one/program.dart
@@ -3,20 +3,33 @@ import 'dart:io';
 void main() {
   print("Grocery Bill");
   print("============");
-  double ricePrice = 2.99;
-  double anotherRicePrice = 1.99;
-  double beansPrice = 3.99;
-  double breadPrice = 2.49;
+  const double ricePrice = 2.99;
+  const double anotherRicePrice = 1.99;
+  const double beansPrice = 3.99;
+  const double breadPrice = 2.49;
 
   print("Enter Rice Quantity: ");
   String riceInput = stdin.readLineSync() as String;
-  int riceQuantity = int.parse(riceInput);
+  final int riceQuantity = int.parse(riceInput);
+  if (riceQuantity < 0) {
+    print("Invalid quantity");
+    return;
+  }
   print("Enter Bean Quantity: ");
   String beanInput = stdin.readLineSync() as String;
   int beansQuantity = int.parse(beanInput);
+
+  if (beansQuantity == 4) {
+    print("Thank you for buying 4 beans");
+  }
   print("Enter Bread Quantity: ");
   String breadInput = stdin.readLineSync() as String;
   int breadQuantity = int.parse(breadInput);
+
+  if (breadQuantity >= 5) {
+    print("You have bought more than 4 breads and get 1 free");
+    breadQuantity = breadQuantity + 1;
+  }
 
   double riceTotal = ricePrice * riceQuantity;
   double anotherRiceTotal = anotherRicePrice * riceQuantity;
