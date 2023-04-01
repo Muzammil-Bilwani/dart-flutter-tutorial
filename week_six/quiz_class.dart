485cf5b62a5add5a4e2f1037dd1b5adbe3e03e95
HamzaNaeem03 <126310544+HamzaNaeem03@users.noreply.github.com>
2023-04-01 17:09:46 +0500

    Muhammad hamza

--- week_six/quiz_class.dart
+++ week_six/quiz_class.dart
@@ -1,5 +1,7 @@
-// Given a list of numbers, write a Dart function that returns the two numbers in the list that add up to a given target value.
-// If there are multiple pairs of numbers that add up to the target value, the function should return the pair with the lowest indices.
+// Given a list of numbers, write a Dart function that returns the two numbers
+//in the list that add up to a given target value.
+// If there are multiple pairs of numbers that add up to the target value, 
+//  the function should return the pair with the lowest indices.
 
 // For example, given the list [1, 2, 3, 4, 5] and a target value of 7,
 // the function should return [2, 5], since 2 + 5 = 7 and
@@ -14,7 +16,17 @@ void main() {
 }
 
 List<int> findPair(List<int> numbers, int target) {
-  // Your code here
-
+ List<int> returnedValue = [numbers , target]{
+  for(int i =0; i>=numbers.length; i++){
+      for(int j=0; j>=numbers.length; j++){
+      
+      if(numbers[i]+numbers[j]==7){
+        print("$numbers[i] , $numbers[j]");
+      } 
+      
+      
+      }
+  }
+}
   return [2, 5];
 }
