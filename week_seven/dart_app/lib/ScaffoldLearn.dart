0e56c286e9402b3e9e207ea21f73188e92c8aece
Muzammil Bilwani <muzammil.bilwani@hotmail.com>
2023-05-06 15:43:56 +0500

    Code updated for scafolld

---
+++ week_seven/dart_app/lib/ScaffoldLearn.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ScafoldLearn extends StatelessWidget {
+  const ScafoldLearn({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('My App'),
+      ),
+      body: Center(
+        child: Text('Welcome to my app!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your code here
+        },
+        elevation: 120,
+        child: Icon(Icons.home),
+      ),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: <Widget>[
+            const DrawerHeader(
+              child: Text('Menu'),
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Add your code here
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Add your code here
+              },
+            ),
+          ],
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 2,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        selectedItemColor: Colors.blue,
+        onTap: (index) {
+          // Add your code here
+        },
+      ),
+    );
+  }
+}
--- week_seven/dart_app/lib/main.dart
+++ week_seven/dart_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:dart_app/Home.dart';
+import 'package:dart_app/ScaffoldLearn.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Home(),
+      home: ScafoldLearn(),
     );
   }
 }
